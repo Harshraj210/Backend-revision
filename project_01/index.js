399e2c78c9703d6f8e551b08c02381503d6e012f
harsh <nh1750501@gmail.com>
2025-10-26 13:35:02 +0530

    getting users details in json

--- project_01/index.js
+++ project_01/index.js
@@ -1,6 +1,13 @@
 import express from "express";
+// it is plain data file
+
+import users from "./MOCK_DATA.json" with { type: "json" };
 
 const app = express()
 const port = 8000
+
+app.get('/users',(req,res)=>{
+  return res.json(users)
+})
 
 app.listen(port,console.log(`server is running at ${port}`))
