16b77c869f8861b586f769c1ac62142f921b73ec
harsh <nh1750501@gmail.com>
2025-10-26 13:59:24 +0530

    getting particular details

--- project_01/index.js
+++ project_01/index.js
@@ -6,8 +6,17 @@ import users from "./MOCK_DATA.json" with { type: "json" };
 const app = express()
 const port = 8000
 
-app.get('/users',(req,res)=>{
+app.get('/api/users',(req,res)=>{
   return res.json(users)
+})
+app.get('/api/users/:id',(req,res)=>{
+ 
+  const id = req.params.id
+  const user= users.find((user)=>user.id===id)
+  if (!user) {
+    return res.status(404).json({ status: "error", message: "User not found" });
+  }
+  return res.json(user)
 })
 
 app.listen(port,console.log(`server is running at ${port}`))
