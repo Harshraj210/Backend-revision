f31c8aa38434998da6f4dec82ba56526c69f5f3d
harsh <nh1750501@gmail.com>
2025-10-26 15:28:13 +0530

    Added post method for registering new user

--- project_01/index.js
+++ project_01/index.js
@@ -5,18 +5,32 @@ import users from "./MOCK_DATA.json" with { type: "json" };
 
 const app = express()
 const port = 8000
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/api/users',(req,res)=>{
-  return res.json(users)
-})
-app.get('/api/users/:id',(req,res)=>{
- 
-  const id = req.params.id
+
+app
+  .route("/api/users/:id")
+  .get((req,res)=>{
+    const id = req.params.id
   const user= users.find((user)=>user.id===id)
   if (!user) {
     return res.status(404).json({ status: "error", message: "User not found" });
   }
   return res.json(user)
-})
+  })
+  .put((req,res)=>{
+    const newUser = req.body
+  })
+  .delete((req,resz)=>{})
+  .patch((req,res)=>{})
+  .post((req,res)=>{
+    const body = req.body
+    console.log("Body",body)
+    return res.json({status:"User registered"})
+  })
+
+
+
+
 
 app.listen(port,console.log(`server is running at ${port}`))
