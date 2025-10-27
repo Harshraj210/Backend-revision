6c498a4097b53b0304663266c69e35d67211d8c2
harsh <nh1750501@gmail.com>
2025-10-27 21:33:43 +0530

    Added all folders

--- project_01/index.js
+++ project_01/index.js
@@ -2,37 +2,40 @@ import express from "express";
 // it is plain data file
 import fs from "fs"
 
-import users from "./MOCK_DATA.json" with { type: "json" };
+import users from "./MOCK_DATA.json" with {type: "json"};
 
 const app = express()
 const port = 8000
 app.use(express.urlencoded({ extended: false }));
 
-
+app.use((req,res,next)=>{
+  console.log("Hello from middleware 1")
+  next()
+})
 app
   .route("/api/users/:id")
-  .get((req,res)=>{
+  .get((req, res) => {
     const id = req.params.id
-  const user= users.find((user)=>user.id===id)
-  if (!user) {
-    return res.status(404).json({ status: "error", message: "User not found" });
-  }
-  return res.json(user)
+    const user = users.find((user) => user.id === id)
+    if (!user) {
+      return res.status(404).json({ status: "error", message: "User not found" });
+    }
+    return res.json(user)
   })
-  .put((req,res)=>{
+  .put((req, res) => {
     const newUser = req.body
   })
-  .delete((req,resz)=>{})
-  .patch((req,res)=>{})
-  .post((req,res)=>{
+  .delete((req, resz) => { })
+  .patch((req, res) => { })
+  .post((req, res) => {
     const body = req.body
-    const newUser = { 
-        id: (users.length + 1).toString(), 
-        ...body 
+    const newUser = {
+      id: (users.length + 1).toString(),
+      ...body
     };
     users.push(newUser)
     fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
-        return res.status(201).json({ status: "User registered", id: newUser.id });
+      return res.status(201).json({ status: "User registered", id: newUser.id });
     });
   })
 
@@ -40,4 +43,4 @@ app
 
 
 
-app.listen(port,console.log(`server is running at ${port}`))
+app.listen(port, console.log(`server is running at ${port}`))
